ffe7c79aaa45ae55d4a7f38b766f620a18de2362
Taha Galata <tgalata2000@gmail.com>
2020-11-05 00:43:11 +0300

    Video list edits

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+import React, {useState} from 'react'
 import './App.scss';
 import Header from './components/header/header'
 import UrlInput from './components/urlInput/urlInput'
 import VideoList from './components/videoList/videoList'
 
 function App() {
+  const [videos, setVideos] = useState("none")
+
   return (
     <>
     <Header />
-    <UrlInput />
-    <VideoList />
+    <UrlInput setVideos= {setVideos}/>
+    {videos !== "none" && 
+    <VideoList videos={videos}/>}
     </>
   );
 }
--- src/components/urlInput/urlInput.js
+++ src/components/urlInput/urlInput.js
@@ -5,7 +5,7 @@ export default function UrlInput() {
   return (
     <section className="section urlSection">
         <div className="container">
-          <h1><span className="red">Youtube</span> Playlist Duration</h1>
+          <h1><span className="red">YouTube</span> Playlist Duration</h1>
           <p>Get the duration of a youtube playlist!</p>
           <form>
             <input 
