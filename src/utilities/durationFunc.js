ad24e030d2efe721a32e67e43ab02a33daee648f
Taha Galata <tgalata2000@gmail.com>
2021-02-15 19:41:25 +0300

    fixed days

--- src/utilities/durationFunc.js
+++ src/utilities/durationFunc.js
@@ -1,13 +1,13 @@
 export function secondsToOthers(duration) {
   let days = 0, hours = 0, minutes = 0, seconds = 0
   days = Math.floor(duration / 86400)
-  let remainingAfterDays = duration % 84600
+  let remainingAfterDays = duration % 86400
   hours = Math.floor(remainingAfterDays / 3600)
   let remainingAfterHours = remainingAfterDays % 3600
   minutes = Math.floor(remainingAfterHours / 60)
   seconds = remainingAfterHours % 60
 
-  return {days, hours, minutes, seconds}  
+  return {days, hours, minutes, seconds} 
 }
 
 export function secondsToLongString(duration) {
@@ -25,11 +25,11 @@ export function secondsToLongString(duration) {
 export function secondsToShortString(duration) {
   let {days, hours, minutes, seconds} = secondsToOthers(duration)
 
-
-  if (hours   < 10) {hours   = "0"+hours;}
-  if (minutes < 10) {minutes = "0"+minutes;}
+  hours += days * 24
+  if (hours   < 10 && days) {hours   = "0"+hours;}
+  if (minutes < 10 && (hours || days)) {minutes = "0"+minutes;}
   if (seconds < 10) {seconds = "0"+seconds;}
 
-  return hours+':'+minutes+':'+seconds;
+  return (hours ? (hours+':') : '')+minutes+':'+seconds;
   
 }
